44c7ea38cf3c802efb982a903548bbd315c6fa33
Andreas Paul <andreas.paul@1und1.de>
2015-05-22 15:37:37 +0200

    add syslog and debug logging

--- gorped.go
+++ gorped.go
@@ -1,19 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
-	//"flag"
+	"log/syslog"
   "strings"
   "syscall"
   "os"
-  //"reflect"
   "strconv"
   "os/exec"
 	"net/http"
-	//"html/template"
 
 	"github.com/xorpaul/gencerts"
-	//"github.com/SlyMarbo/spdy"
   "code.google.com/p/gcfg"
   "github.com/kballard/go-shellquote"
 )
@@ -32,44 +30,45 @@ type CheckResult struct {
 }
 
 func httpHandler(w http.ResponseWriter, req *http.Request) {
-	log.Print("Request: ", req)
+	Debugf("Request: ", req)
   ip := strings.Split(req.RemoteAddr,":")[0]
-	log.Print("Incoming request from IP: ", ip)
-	log.Print("Request path: ", req.URL.Path)
+	Debugf("Incoming request from IP: ", ip)
+	Debugf("Request path: ", req.URL.Path)
 
   reqParts := strings.Split(req.URL.Path[1:], "/")
-  log.Printf("Request path parts are: %q", reqParts)
+  Debugf("Request path parts are: %q", reqParts)
   command := reqParts[0]
-  log.Print("Found command: ", command)
+  Debugf("Found command: ", command)
   cmdArguments := reqParts[1:len(reqParts)]
-  log.Printf("Found command arguments: %q", cmdArguments)
+  Debugf("Found command arguments: %q", cmdArguments)
 
 	w.Header().Set("Content-Type", "text/plain")
 
   if _, ok := commandsCfgSection[command]; ok {
     argCount := strings.Count(commandsCfgSection[command], "$ARG$")
-    log.Printf("Found %q command arguments in this command", len(cmdArguments))
+    Debugf("Found %q command arguments in this command", len(cmdArguments))
     if argCount > len(cmdArguments) {
       w.Write([]byte("Too few command arguments!"))
     } else {
       cmdString := commandsCfgSection[command]
-      log.Print("Got command from config: ", cmdString)
+      Debugf("Got command from config: ", cmdString)
       for _, arg := range cmdArguments {
         // TODO: filter nasty chars
         if arg != "" {
           cmdString = strings.Replace(cmdString, "$ARG$", arg, 1)
-          log.Printf("Replacing $ARG$ with %q results in %q", arg, cmdString)
+          Debugf("Replacing $ARG$ with %q results in %q", arg, cmdString)
         }
       }
-      log.Print("Replacing arguments and executing: ", cmdString)
+      Debugf("Replacing arguments and executing: ", cmdString)
       cr := execCommand(cmdString)
-      log.Print("Last char: ", cr.text[len(cr.text)-1])
+      Debugf("Last char: ", cr.text[len(cr.text)-1])
       // Making sure that the check script output ends with a newline char
       if cr.text[len(cr.text)-1] != 10 {
         cr.text = append(cr.text, "\n"...)
       }
       output := append(cr.text, "Returncode: "...)
       output = strconv.AppendInt(output, int64(cr.returncode), 10)
+      output = append(output, "\n"...)
       w.Write(output)
     }
   } else {
@@ -89,30 +88,30 @@ func execCommand(cmdString string) CheckResult {
     //checkArguments, err := shellquote.Split(strings.Join(parts[1:len(parts)], " "))
     foobar, err := shellquote.Split(parts[1])
     if err != nil {
-      log.Print("err: ", err)
+      Debugf("err: ", err)
     } else {
       checkArguments = foobar
     }
-    log.Printf("checkArguments are %q: ", checkArguments)
+    Debugf("checkArguments are %q: ", checkArguments)
   }
-  log.Print("checkScript: ", checkScript)
-    log.Printf("checkArguments are %q: ", checkArguments)
+  Debugf("checkScript: ", checkScript)
+    Debugf("checkArguments are %q: ", checkArguments)
   //if _, err := os.Stat(checkScript); os.IsNotExist(err) {
   //  return CheckResult{[]byte("UKNOWN: unknown output"), 3}
   //}
   //info, _ := os.Stat(checkScript)
   //mode := info.Mode()
-  //log.Print("mode: ", mode & 0111)
-  //log.Print("arg1: ", checkArguments[len(checkArguments)-1])
+  //Debugf("mode: ", mode & 0111)
+  //Debugf("arg1: ", checkArguments[len(checkArguments)-1])
 
   out, err := exec.Command(checkScript, checkArguments...).Output()
 
   if err != nil {
-    log.Print("err: ", err)
+    Debugf("err: ", err)
     if out == nil {
       out = []byte("UKNOWN: unknown output\n")
     }
-    log.Print("out: ", string(out))
+    Debugf("out: ", string(out))
   } else {
     if msg, ok := err.(*exec.ExitError); ok { // there is error code
       returncode = msg.Sys().(syscall.WaitStatus).ExitStatus()
@@ -121,8 +120,8 @@ func execCommand(cmdString string) CheckResult {
     }
   }
 
-	log.Print("Got output: ", string(out[:]))
-	log.Print("Got return code: ", returncode)
+	Debugf("Got output: ", string(out[:]))
+	Debugf("Got return code: ", returncode)
   //return template.HTMLEscapeString(string(out))
   return CheckResult{out, returncode}
 }
@@ -144,52 +143,76 @@ func readConfigfile(configFile string) ConfigSettings {
 	}
 
   cfgMain := &cfg.Main
-  log.Print("Found main config settings:")
   for _, n := range cfgMain.Names() {
-    log.Print(n, " = ", *cfgMain.Vals[cfgMain.Idx(n)])
     mainCfgSection[n] = *cfgMain.Vals[cfgMain.Idx(n)]
+  }
+
+  Debugf("Found main config settings:")
+  for _, n := range cfgMain.Names() {
+    Debugf(n, " = ", *cfgMain.Vals[cfgMain.Idx(n)])
   }
 
   cfgCommands := &cfg.Commands
   // Names(): iterate over variables with undefined order and case
-  log.Print("Found commands config settings:")
+  Debugf("Found commands config settings:")
   for _, n := range cfgCommands.Names() {
-    log.Print(n, " = ", *cfgCommands.Vals[cfgCommands.Idx(n)])
+    Debugf(n, " = ", *cfgCommands.Vals[cfgCommands.Idx(n)])
     commandsCfgSection[n] = *cfgCommands.Vals[cfgCommands.Idx(n)]
   }
 
   return ConfigSettings{mainCfgSection, commandsCfgSection}
 }
 
+func Debugf(format string, args ...interface{}) {
+    if mainCfgSection["debug"] != "0" {
+        log.Print("DEBUG " + format, args)
+    }
+}
+
 func main() {
 
-  configSettings := readConfigfile("config.gcfg")
+  // http://technosophos.com/2013/09/14/using-gos-built-logger-log-syslog.html
+  // Configure logger to write to the syslog.
+  logwriter, e := syslog.New(syslog.LOG_NOTICE, "gorpe")
+  if e == nil {
+    log.SetOutput(logwriter)
+  }
+
+  var configFile = flag.String("config", "/etc/gorpe/gorpe.gcfg", "which config file to use at startup, defaults to /etc/gorpe/gorpe.gcfg")
+
+  if _, err := os.Stat(*configFile); os.IsNotExist(err) {
+    log.Printf("could not find config file: %s", *configFile)
+    os.Exit(1)
+  }
+
+  log.Print("using config file: ", *configFile)
+  configSettings := readConfigfile(*configFile)
   log.Print("found commands: ", configSettings.commands)
 
 
   // check if we need to generate certificates
   var certFilenames = map[string]string{
-    "cert": "./cert.pem",
-    "key": "./key.pem",
+    "cert": mainCfgSection["certs_dir"] + "/cert.pem",
+    "key": mainCfgSection["certs_dir"] + "/key.pem",
   }
 
   for _, filename := range certFilenames {
     if _, err := os.Stat(filename); os.IsNotExist(err) {
       // generate certs
-      log.Print("Certificate file: ", filename, " not found! Generating certificate...\n")
-      gencerts.GenerateCert()
+      Debugf("Certificate file: ", filename, " not found! Generating certificate...\n")
+      gencerts.GenerateCert(certFilenames["cert"], certFilenames["key"])
       break
     } else {
-      log.Print("Certificate file: ", filename, " found. Skipping certificate generation\n")
+      Debugf("Certificate file: ", filename, " found. Skipping certificate generation\n")
     }
   }
 
 
 	http.HandleFunc("/", httpHandler)
-	log.Printf("Listening on port %s. Go to https://%s:%s/", configSettings.main["server_port"], configSettings.main["server_address"], configSettings.main["server_port"])
+	log.Printf("Listening on https://%s:%s/", configSettings.main["server_address"], configSettings.main["server_port"])
   //err := spdy.ListenAndServeSpdyOnly(":"+configSettings.main["server_port"], certFilenames["cert"], certFilenames["key"], nil)
-  //err := http.ListenAndServeTLS(":"+configSettings.main["server_port"], certFilenames["cert"], certFilenames["key"], nil)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
+  err := http.ListenAndServeTLS(":"+configSettings.main["server_port"], certFilenames["cert"], certFilenames["key"], nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
