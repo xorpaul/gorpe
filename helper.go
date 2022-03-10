f2f4ba5935e4b3f1e83cf89f0f067a674db91516
Andreas Paul <xorpaul@gmail.com>
2022-03-10 13:22:50 +0100

    use xorpaul/gohelper InBetween()

--- helper.go
+++ helper.go
@@ -21,7 +21,7 @@ type ExecResult struct {
 
 // Exit is a helper function to output a check result in a standardized way
 func (cr checkResult) Exit(w http.ResponseWriter) {
-	if !(cr.returncode != 0 || cr.returncode != 1 || cr.returncode != 2 || cr.returncode != 3) {
+	if !h.InBetween(cr.returncode, 0, 3) {
 		cr.returncode = 3
 	}
 	w.Header().Set("Content-Type", "text/plain")
