e1fa3624fda61654d1efc28d2ad64da215e7cfb2
Andreas Paul <xorpaul@gmail.com>
2025-03-13 17:10:30 +0100

    use new build_release.sh

--- build_release.sh
+++ build_release.sh
@@ -1,73 +1,54 @@
 #! /usr/bin/env bash
-#set -e
+set -e
 
 if [ $# -ne 2 ]; then
   echo "need the version number and release comment as argument"
   echo "e.g. ${0} 0.4.5 'fix local modules and modules with install_path purging bug #80 #82'"
   echo "Aborting..."
-	exit 1
+  exit 1
 fi
-
+#
 time go test -v
-
-if [ $? -ne 0 ]; then 
-  echo "Tests unsuccessfull"
+#
+# Remove leading 'v' from the version number if present
+version=${1#v}
+#
+if [ $? -ne 0 ]; then
+  echo "Tests unsuccessful"
   echo "Aborting..."
-	exit 1
+  exit 1
 fi
-
-# try to get the project name from the current working directory
-projectname=${PWD##*/}
-echo "using ${projectname} for projectname"
-
-#sed -i "s/${projectname} version [^ ]*/${projectname} version ${1}/" ${projectname}.go
-#git add ${projectname}.go
-#git commit -m "bump version to v${1}"
-
+#
+#
 echo "creating git tag v${1}"
 git tag v${1}
 echo "pushing git tag v${1}"
 git push -f --tags
 git push
 
-rm ${projectname}
+# try to get the project name from the current working directory
+projectname=${PWD##*/}
+upx=$(which upx)
+export CGO_ENABLED=0
+export BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S')
+export BUILDVERSION=$(git describe --tags)
 
-echo "creating github release v${1}"
-github-release release --user xorpaul     --repo ${projectname}     --tag v${1}     --name "v${1}"     --description "${2}"
+build() {
+  echo "building ${projectname}-$1-$2 with version ${version}"
+  env GOOS=$1 GOARCH=$2 go build -ldflags "-X main.buildtime=${BUILDTIME} -X main.buildversion=${BUILDVERSION}"
+  if [ ${#upx} -gt 0 ]; then
+    if [ $1 == "linux" ]; then
+      $upx ${projectname}
+    fi
+  fi
+  zip ${projectname}-v${version}-$1-$2.zip ${projectname}
+}
 
-upx=`which upx`
+for os in darwin linux; do
+  for arch in arm64 amd64; do
+    build $os $arch
+  done
+done
 
-if [ ${#upx} -gt 0 ]; then
-  echo "building and uploading ${projectname}-darwin-amd64-debug"
-  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') BUILDVERSION=$(git describe --tags) && env GOOS=darwin GOARCH=amd64 go build -ldflags "-X main.buildtime=$BUILDTIME_v${1} -X main.buildversion=${BUILDVERSION}" && date
-  zip ${projectname}-darwin-amd64-debug.zip ${projectname}
-  github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-darwin-amd64-debug.zip" --file ${projectname}-darwin-amd64-debug.zip
-fi
-
-echo "building and uploading ${projectname}-darwin-amd64"
-BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') BUILDVERSION=$(git describe --tags) && env GOOS=darwin GOARCH=amd64 go build -ldflags "-s -w -X main.buildtime=$BUILDTIME_v${1} -X main.buildversion=${BUILDVERSION}" && date
-if [ ${#upx} -gt 0 ]; then
-  $upx --brute ${projectname}
-fi
-zip ${projectname}-darwin-amd64.zip ${projectname}
-github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-darwin-amd64.zip" --file ${projectname}-darwin-amd64.zip
-
-
-
-
-
-if [ ${#upx} -gt 0 ]; then
-  echo "building and uploading ${projectname}-linux-amd64-debug"
-  BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') BUILDVERSION=$(git describe --tags) && go build -race -ldflags "-X main.buildtime=$BUILDTIME_v${1} -X main.buildversion=${BUILDVERSION}" && date && ./${projectname} --version
-  zip ${projectname}-linux-amd64-debug.zip ${projectname}
-  github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-linux-amd64-debug.zip" --file ${projectname}-linux-amd64-debug.zip
-fi
-
-echo "building and uploading ${projectname}-linux-amd64"
-BUILDTIME=$(date -u '+%Y-%m-%d_%H:%M:%S') BUILDVERSION=$(git describe --tags) && go build -race -ldflags "-s -w -X main.buildtime=$BUILDTIME_v${1} -X main.buildversion=${BUILDVERSION}" && date && ./${projectname} --version
-if [ ${#upx} -gt 0 ]; then
-  $upx --brute ${projectname}
-fi
-zip ${projectname}-linux-amd64.zip ${projectname}
-github-release upload     --user xorpaul     --repo ${projectname}     --tag v${1}     --name "${projectname}-linux-amd64.zip" --file ${projectname}-linux-amd64.zip
-
+test -z ${GITHUB_TOKEN} || echo "creating github release v${1}"
+test -z ${GITHUB_TOKEN} && echo "skipping github-release as GITHUB_TOKEN is not set" || gh release create --fail-on-no-commits --verify-tag --repo ${projectname} --title "v${1}" --notes "${2}" v${1} ./${projectname}-v${1}*.zip
